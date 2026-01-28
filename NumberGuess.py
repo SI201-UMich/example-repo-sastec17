b18dc6b17ece589a3d0a21b1220ddf5278a100ed
Sarah Stec <sastec@umich.edu>
2026-01-28 15:31:39 -0500

    Added a greetinggit status

--- NumberGuess.py
+++ NumberGuess.py
@@ -1,20 +1,23 @@
 import random
+
+
 class NumberGuess:
-    
     def __init__(self):
         self.number = random.randint(1, 100)
         self.guess = -1
-        
+        print("Hi!!!!")
+
     def play(self):
-        while (self.number != self.guess):
+        while self.number != self.guess:
             value = input("Guess a number from 1 to 100: ")
             self.guess = int(value)
-            if (self.guess < self.number):
+            if self.guess < self.number:
                 print("Too low")
-            elif (self.guess > self.number):
+            elif self.guess > self.number:
                 print("Too high")
             else:
                 print("You guessed the number!")
-                
+
+
 game = NumberGuess()
 game.play()
